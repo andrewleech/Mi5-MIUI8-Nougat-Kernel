c2e902c735f35a47986a787e9c4d555e1f12d293
Andrew Leech <andrew@alelec.net>
2017-02-28 10:09:44 +1100

    Add veritymode == enforcing to command line

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -5,6 +5,7 @@
 #include <asm/setup.h>
 
 static char proc_cmdline[COMMAND_LINE_SIZE];
+static char proc_cmdline_temp[COMMAND_LINE_SIZE];
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
@@ -36,12 +37,28 @@ static int __init proc_cmdline_init(void)
 		if (!a2) /* last argument on the cmdline */
 			a2 = "";
 
-		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*sgreen%s",
+		scnprintf(proc_cmdline_temp, COMMAND_LINE_SIZE, "%.*sgreen%s",
 			  (int)(a1 - saved_command_line + 1),
 			  saved_command_line, a2);
 	} else {
-		strncpy(proc_cmdline, saved_command_line, COMMAND_LINE_SIZE);
+		strncpy(proc_cmdline_temp, saved_command_line, COMMAND_LINE_SIZE);
 	}
+
+	a1 = strstr(proc_cmdline_temp, "androidboot.veritymode=");
+	if (a1) {
+		a1 = strchr(a1, '=');
+		a2 = strchr(a1, ' ');
+		if (!a2) // last argument on the cmdline
+			a2 = "";
+
+		scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%.*senforcing%s",
+			  (int)(a1 - proc_cmdline_temp + 1),
+			  proc_cmdline_temp, a2);
+	} else {
+		strncpy(proc_cmdline, proc_cmdline_temp, COMMAND_LINE_SIZE);
+	}
+
+//        scnprintf(proc_cmdline, COMMAND_LINE_SIZE, "%s androidboot.bl_state=0 androidboot.flash.locked=1", proc_cmdline_temp);
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
